142ccaade4aecbebc118be4fed327840f00f30c1
Tech-Zee <azeezatokunola06@gmail.com>
2026-06-10 15:39:30 +0100

    describe what you changed

--- src/User/components/about/hero.jsx
+++ src/User/components/about/hero.jsx
@@ -1,7 +1,15 @@
-
+import styles from "./hero.module.css"
 
 function AboutHero(){
-
+    return(
+        <>
+           <div className={styles.Herocontainer}>
+                <p className={styles.heroText}>
+                    "More than just Footwear - <br /> We're here to help you step confidently into every moment"
+                </p>
+           </div>
+        </>
+    )
 }
 
 export default AboutHero;
